471f12e6a55bf615d5a3f42d4485b73ee8b42ed1
Oleinic Ion <ion.oleinic21@gmail.com>
2023-10-28 14:32:54 +0300

    changed smart strip sensor behavior

--- src/pages/Devices/Device/SmartStrip/SmartStrip.js
+++ src/pages/Devices/Device/SmartStrip/SmartStrip.js
@@ -2,51 +2,34 @@ import React, { useState, useEffect } from 'react'
 import './SmartStrip.css'
 import Switch from './Switch/Switch'
 import { app } from '../../../api/api'
+import voltageIcon from './SensorDataIcons/voltage-icon.png'
+import currentIcon from './SensorDataIcons/current-icon.png'
+import powerIcon from './SensorDataIcons/power-icon.png'
+import totalPowerIcon from './SensorDataIcons/total-power-icon.png'
 
-const sensor_data_demo = {
-  StatusSNS: {
-    Time: '0000-00-00T00:00:00',
-    ENERGY: {
-      TotalStartTime: '2022-10-13T16:13:07',
-      Total: '-:-',
-      Yesterday: '-:-',
-      Today: '-:-',
-      Power: '-:-',
-      ApparentPower: '-:-',
-      ReactivePower: '-:-',
-      Factor: '-:-',
-      Voltage: '-:-',
-      Current: '-:-',
-    },
-  },
-}
 function SmartStrip({ device, visibility }) {
-  let init_statuses = []
+  let initStatuses = []
   let initCheckedlist = []
+  let sensorPart = <></>
+  let switches = []
   for (let i = 0; i < device.nr_of_sockets; i++) {
-    init_statuses.push('OFF')
+    initStatuses.push('OFF')
     initCheckedlist.push(false)
   }
-  const [statusList, setStatusList] = useState(init_statuses)
+  const [statusList, setStatusList] = useState(initStatuses)
   const [isCheckedList, setIsCheckedList] = useState(initCheckedlist)
-  const [sensorData, setSensorData] = useState(sensor_data_demo)
+  const [sensorData, setSensorData] = useState({
+    Total: '-:-',
+    Today: '-:-',
+    Power: '-:-',
+    Voltage: '-:-',
+    Current: '-:-',
+  })
 
-  const updateStatuses = (power_status) => {
-    setStatusList(power_status)
-    for (let i = 0; i < power_status.length; i++) {
-      if (power_status[i] === 'ON') {
-        isCheckedList[i] = true
-        setIsCheckedList(isCheckedList)
-      } else {
-        isCheckedList[i] = false
-        setIsCheckedList(isCheckedList)
-      }
-    }
-  }
   useEffect(() => {
     updateStatuses(device.power_status)
     if (device.switch_type == 'plug') {
-      setSensorData(device.sensor_status)
+      setSensorData(device.sensor_data)
     }
   }, [device])
   const sensorUpdateReq = async () => {
@@ -71,6 +54,18 @@ function SmartStrip({ device, visibility }) {
       clearInterval(interval)
     }
   }, [])
+  const updateStatuses = (power_status) => {
+    setStatusList(power_status)
+    for (let i = 0; i < power_status.length; i++) {
+      if (power_status[i] === 'ON') {
+        isCheckedList[i] = true
+        setIsCheckedList(isCheckedList)
+      } else {
+        isCheckedList[i] = false
+        setIsCheckedList(isCheckedList)
+      }
+    }
+  }
   const sendChangePower = async (socket_nr, pwr_status) => {
     const response = await app.post(
       `/smartStrip?status=${pwr_status}&device_id=${device.id}&socket_nr=${
@@ -90,37 +85,34 @@ function SmartStrip({ device, visibility }) {
       console.log(err.message)
     }
   }
-  let ENERGY = sensor_data_demo.StatusSNS.ENERGY
-  if (sensorData.StatusSNS) {
-    ENERGY = sensorData.StatusSNS.ENERGY
-  }
-  let sensor_part = <></>
+  //init sensor part (ENERGY)
   if (device.switch_type === 'plug') {
-    sensor_part = (
+    sensorPart = (
       <div
         className='sensor'
-        style={{ display: ENERGY === undefined ? 'none' : 'flex' }}
+        style={{ display: sensorData === undefined ? 'none' : 'flex' }}
       >
         <div className='sensor-item'>
-          <img src='https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-voltage-electrician-flaticons-lineal-color-flat-icons-15.png' />
-          <p>{ENERGY.Voltage} V</p>
+          <img src={voltageIcon} />
+          <p>{sensorData.Voltage} V</p>
         </div>
         <div className='sensor-item' style={{ marginLeft: '-0.5em' }}>
-          <img src='https://img.icons8.com/emoji/48/null/high-voltage.png' />
-          <p>{ENERGY.Current} A</p>
+          <img src={currentIcon} />
+          <p>{sensorData.Current} A</p>
         </div>
         <div className='sensor-item'>
-          <img src='https://cdn-icons-png.flaticon.com/512/5387/5387682.png' />
-          <p>{ENERGY.Power} W</p>
+          <img src={powerIcon} />
+          <p>{sensorData.Power} W</p>
         </div>
         <div className='sensor-item' style={{ width: '50%' }}>
-          <img src='https://cdn2.iconfinder.com/data/icons/unit-of-measurement/500/measurement-unit_12-512.png' />
-          <p>{ENERGY.Total} kW</p>
+          <img src={totalPowerIcon} />
+          <p>{sensorData.Total} kW</p>
         </div>
       </div>
     )
   }
-  let switches = []
+
+  //init switches
   for (let i = 0; i < device.nr_of_sockets; i++) {
     switches.push(
       <Switch
@@ -131,6 +123,7 @@ function SmartStrip({ device, visibility }) {
       />
     )
   }
+
   return (
     <div
       className='smart-strip'
@@ -144,11 +137,11 @@ function SmartStrip({ device, visibility }) {
             display: device.switch_type === 'plug' ? 'flex' : 'none',
           }}
         >
-          <h1>{ENERGY.Today}</h1>
+          <h1>{sensorData.Today}</h1>
           <p>kW</p>
         </div>
       </div>
-      {sensor_part}
+      {sensorPart}
     </div>
   )
 }
